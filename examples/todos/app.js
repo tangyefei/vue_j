4eb0366e302f5debddbb112d72baf12c2bf2d632
TangYefei Tang <tyfccsu@gmail.com>
2016-10-18 21:06:20 +0800

    ms_03 toggle, toggleAll and edit function implementation

--- examples/todos/app.js
+++ examples/todos/app.js
@@ -11,11 +11,15 @@ Seed.controller('Todos', function(scope){
 
     // regular properties -----------------------------------------------------
     scope.todos = todos;    
+    scope.allDone = false
     scope.remaining = todos.reduce(function (count, todo) {
         return count + (todo.done ? 0 : 1)
     }, 0)
 
     // event handlers ---------------------------------------------------------
+    scope.total = function () {
+        return scope.todos.length
+    }
     scope.addTodo = function (e) {
         var val = e.el.value
         if (val) {
@@ -23,8 +27,26 @@ Seed.controller('Todos', function(scope){
             scope.todos.unshift({ text: val, done: false })
         }
         scope.remaining++
-        console.log(scope.todos)
-        console.log(scope.remaining)
+    }
+
+    scope.updateCount = function (e) {
+        scope.remaining += e.scope.done ? -1 : 1
+        scope.allDone = scope.remaining === 0
+    }
+
+    scope.edit = function (e) {
+        e.scope.editing = true
+    }
+
+    scope.stopEdit = function (e) {
+        e.scope.editing = false
+    }
+
+    scope.toggleAll = function (e) {
+        scope.todos.forEach(function (todo) {
+            todo.done = e.el.checked
+        })
+        scope.remaining = e.el.checked ? 0 : scope.total()
     }
 })
 
--- src/directives/index.js
+++ src/directives/index.js
@@ -1,6 +1,10 @@
 module.exports = {
     on: require('./on'),
 
+    focus: function (value) {
+        this.el[value ? 'focus' : 'blur']()
+    },
+    
     class: function (value) {
         this.el.classList[value ? 'add' : 'remove'](this.arg)
     },
@@ -9,6 +13,38 @@ module.exports = {
         this.el.textContent =
             (value !== null && value !== undefined)
             ? value.toString() : ''
+    },
+
+    value: {
+        bind: function () {
+            var el = this.el, self = this
+            this.change = function () {
+                self.seed.scope[self.key] = el.value
+            }
+            el.addEventListener('change', this.change)
+        },
+        update: function (value) {
+            this.el.value = value
+        },
+        unbind: function () {
+            this.el.removeEventListener('change', this.change)
+        }
+    },
+
+    checked: {
+        bind: function () {
+            var el = this.el, self = this
+            this.change = function () {
+                self.seed.scope[self.key] = el.checked
+            }
+            el.addEventListener('change', this.change)
+        },
+        update: function (value) {
+            this.el.checked = !!value
+        },
+        unbind: function () {
+            this.el.removeEventListener('change', this.change)
+        }
     },
 
     each: require('./each')
